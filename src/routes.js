d245a7b73f014c41012d4d67895757f8194c5c27
mdaniksiddikabir <67305440+mdaniksiddikabir@users.noreply.github.com>
2026-02-20 07:05:59 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useAuth } from './context/AuthContext';
 
 // Pages
 import HomePage from './pages/HomePage';
@@ -8,18 +9,28 @@ import QiblaPage from './pages/QiblaPage';
 import QuranPage from './pages/QuranPage';
 import CalendarPage from './pages/CalendarPage';
 import DuaPage from './pages/DuaPage';
-import ProfilePage from './pages/ProfilePage';
 import SettingsPage from './pages/SettingsPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './components/user/Register';
+import ProfilePage from './pages/ProfilePage';
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+import ForgotPassword from './components/user/ForgotPassword';
+import ResetPassword from './components/user/ResetPassword';
 import NotFoundPage from './pages/NotFoundPage';
 
 // Components
 import ProtectedRoute from './components/common/ProtectedRoute';
+import Loader from './components/common/Loader';
 
 const AppRoutes = () => {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <Loader fullScreen />;
+  }
+
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<HomePage />} />
       <Route path="/prayer" element={<PrayerPage />} />
       <Route path="/qibla" element={<QiblaPage />} />
@@ -27,16 +38,21 @@ const AppRoutes = () => {
       <Route path="/calendar" element={<CalendarPage />} />
       <Route path="/duas" element={<DuaPage />} />
       <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route 
-        path="/profile" 
-        element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        } 
-      />
+      
+      {/* Auth Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      
+      {/* Protected Routes */}
+      <Route path="/profile" element={
+        <ProtectedRoute>
+          <ProfilePage />
+        </ProtectedRoute>
+      } />
+      
+      {/* 404 Route */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );

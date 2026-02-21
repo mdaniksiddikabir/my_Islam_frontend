84e7b40ca352ced9db08af4adc8b857d07f7ac7a
mdaniksiddikabir <67305440+mdaniksiddikabir@users.noreply.github.com>
2026-02-22 06:21:10 +0800

    Create RamadanPage.js

---
+++ src/pages/RamadanPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RamadanCalendar from '../components/ramadan/RamadanCalendar';
+
+const RamadanPage = () => {
+  return (
+    <div className="space-y-6">
+      <RamadanCalendar />
+    </div>
+  );
+};
+
+export default RamadanPage;
